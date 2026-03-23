4ce61c390b71c9755b96e4518760e4bf725c9aa0
Ob22a <Ob22adegefu123@gmail.com>
2026-03-23 18:50:00 +0300

    corrected solution

--- B_Two_Cakes.py
+++ B_Two_Cakes.py
@@ -1,6 +1,6 @@
 n, a, b = map(int, input().split())
 
 for x in range((a + b) // n, 0, -1):
-    if (a // x) + (b // x) >= n:
+    if (a//x)>0 and (b//x)>0 and (a // x) + (b // x) >= n:
         print(x)
         break
